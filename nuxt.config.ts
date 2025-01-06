26f5945bc17a799ee3c88152eef6e8dfd64b3f26
Henry Bley-Vroman <olets@olets.dev>
2025-01-05 15:43:35 -1000

    feat(unocss): add and configure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
+import presetCSS from "@olets/unocss-preset-css";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: ["@unocss/nuxt"],
+    // @ts-ignore
+    presets: [presetCSS()],
+  },
+});
