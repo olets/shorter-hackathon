1598041076754ebee91aa144ad30029231cc4eef
Henry Bley-Vroman <olets@olets.dev>
2025-01-05 15:45:43 -1000

    feat(landing page): first pass

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,25 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@unocss/nuxt"],
+  unocss: {
+    preflights: [
+      {
+        getCSS: () => `
+          :root {
+            --border-width: 1px;
+            --color-background: #000;
+            --color-foreground: #fff;
+            --width-fancy-corners: 20px;
+          }
+        `,
+      },
+    ],
     // @ts-ignore
     presets: [presetCSS()],
+    nuxtLayers: true,
+    safelist: [
+      "{background:var(--color-background)}",
+      "{color:var(--color-foreground)}",
+    ],
   },
 });
