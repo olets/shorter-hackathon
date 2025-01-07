6fbce7cad4e0ef9ee8991397899cf43f3ac09d6f
Henry Bley-Vroman <olets@olets.dev>
2025-01-07 11:35:57 -1000

    feat(Header): polish layout

--- components/ColorScheme.vue
+++ components/ColorScheme.vue
@@ -29,13 +29,14 @@ const options = [
 <template>
   <div>
     <label
+      class="@media(min-width:48rem){display:block}"
       :for="runtimeConfig.public.ids.colorScheme"
     >
       Theme:
     </label>
 
     <select
-      class="{border:1px_solid_currentColor} {border-radius:4px} :focus-visible{background-color:color-mix(in_srgb,currentColor_20%,transparent)} :hover{background-color:color-mix(in_srgb,currentColor_20%,transparent)}"
+      class="{border:1px_solid_currentColor} {border-radius:4px} {padding:0.25rem} :focus-visible{background-color:color-mix(in_srgb,currentColor_20%,transparent)} :hover{background-color:color-mix(in_srgb,currentColor_20%,transparent)}"
       :id="runtimeConfig.public.ids.colorScheme"
       v-model="colorScheme.value"
     >
--- components/Header.vue
+++ components/Header.vue
@@ -4,9 +4,9 @@ import { ColorScheme } from "#components";
 
 <template>
   <header
-    class="{display:flex} {gap:0.5rem} {background:var(--color-background)} {border:1px_solid_currentColor} {padding:0.5rem} {position:fixed} {bottom:1.5rem} @media(min-width:48rem){bottom:auto} @media(min-width:48rem){top:1.5rem} {right:1.5rem} {align-items:end} @media(min-width:48rem){flex-direction:column}"
+    class="{display:flex} {align-items:center} @media(min-width:48rem){align-items:start} @media(min-width:48rem){flex-direction:column} {gap:0.5rem} {background-color:color-mix(in_srgb,var(--color-background)_90%,transparent)} {border:1px_solid_currentColor} {padding:0.5rem} {position:fixed} {bottom:1.5rem} @media(min-width:48rem){bottom:auto} @media(min-width:48rem){top:1.5rem} {right:1.5rem} {backdrop-filter:blur(1px)}"
   >
-    <nav class="{display:inherit} {gap:inherit} {flex-direction:inherit}">
+    <nav class="{display:flex} {gap:inherit}">
       <NuxtLink to="/">Home</NuxtLink>
       <NuxtLink to="/about">About</NuxtLink>
     </nav>
